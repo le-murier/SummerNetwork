1bdc8fe44615530936e5414d698a4b8900cea65a
Ihor Pavliuk <nemuzora.nozomu@gmail.com>
2022-07-21 02:50:08 +0300

    Renamed folders, put images into assets folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
