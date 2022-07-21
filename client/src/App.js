5efb0b9c99fc08eff9d66ea65352e9935d1266e5
Ihor Pavliuk <nemuzora.nozomu@gmail.com>
2022-07-21 03:21:51 +0300

    Added routes enum

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Page404 from "./components/Page404/Page404";
 import { Route, Routes } from 'react-router-dom';
+import { AppPath } from "./common/enums/enums";
 
 function App() {
   return (
+    // We might need this StackOverflow answer later to display some components 
+    // across all pages except for login/signup/something else: shorturl.at/deHNW
     <Routes>
-      <Route path="/log-in" element={<Login />} />
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Page404 />} />
+      <Route path={AppPath.SIGN_IN} element={<Login />} />
+      {/* Replace root one with <Home /> when we implement home component */}
+      <Route path={AppPath.ROOT} element={<Login />} />
+      <Route path={AppPath.ANY} element={<Page404 />} />
     </Routes>
   );
 }
